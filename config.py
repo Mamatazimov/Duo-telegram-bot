2f53492e89ea204b7fd4abebadf71d8384ea52ad
Azdev <118669421+Mamatazimov@users.noreply.github.com>
2025-01-31 01:19:56 +0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-Bot_Token = "7861655553:AAH0lSneG0n9S1VR4Zpz4fQuqqrtRVpoLlk"
-Admin_Id = 5514106574
-category_list = ["romance", "isekai", "sports", "harem", "fantasy", "drama", "psychological", "action", "shonen", "seinen", "shojo", "adventure", "comedy", "school", "magic"]
-
-
-
